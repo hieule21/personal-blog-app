ca5fd6a4c61efb8419ee351ecab358dda5a784ea
Hieu Van Le <hieule20901@gmail.com>
2020-06-11 08:48:10 -0500

    upgrade ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,12 +24,16 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
-    "gatsby-plugin-antd",
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-antd`,
+    `gatsby-plugin-mdx`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [{ resolve: `gatsby-remark-prismjs` }],
+      },
+    },
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,23 @@
 import React from "react"
 import "./css/index.css"
-import About from "../components/About/About"
+import About from "../components/about/about"
 
-import { Row, Col, Card, Divider } from "antd"
+import { Row, Col, Card } from "antd"
 import SEO from "../components/seo"
 
 const IndexPage = () => {
   return (
     <React.Fragment>
       <SEO title="Home" />
+
       <Row type="flex" justify="space-around">
         <Col lg={6} md={24} className="about">
-          <Card className="card-shadow">
+          <Card className="card-about">
             <About />
           </Card>
         </Col>
         <Col lg={16} md={24} className="blog">
-          <Card className="card-shadow">Something</Card>
+          <Card className="card-blog">Something</Card>
         </Col>
       </Row>
     </React.Fragment>
